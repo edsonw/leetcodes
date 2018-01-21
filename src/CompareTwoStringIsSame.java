169aab670e4032328a8a4891bf7c9327dfc8376b
edsonw <969693577@qq.com>
2018-01-21 16:49:08 +0800

    面试遇到的

--- src/CompareTwoStringIsSame.java
+++ src/CompareTwoStringIsSame.java
@@ -1,6 +1,8 @@
 /**
  * Created by woo23 on 2018/1/18.
+ * 解法1： （两重循环）。遍历str1的每一个元素和str2的进行比较，因为是可以循环的，所以当遍历str1到尾部但是str2还没到尾部的时候需要对index进行调整。
  *
+ * 解法2：
  */
 public class CompareTwoStringIsSame {
     // TODO: 2018/1/18 比较两个字符串是否是同可以经过变换后相同的例如 abcbaa 和aaabcb是相同的
