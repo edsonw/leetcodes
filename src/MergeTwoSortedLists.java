64f28a6610a41574bb4fb19776578bda3a0827a9
969693577@qq.com <969693577@qq.com>
2018-02-20 10:19:41 +0800

    21 easy

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -10,14 +10,19 @@ public class MergeTwoSortedLists {
       }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
         ListNode pre = new ListNode(0);
         ListNode dummyNode = new ListNode(0);
         dummyNode = pre;
-        if(l1==null)
+        if (l1 == null && l2 == null) {
             return null;
-        if(l2==null)
-            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
         if(l1.val<=l2.val)
         {
             pre.next = l1;
@@ -28,23 +33,28 @@ public class MergeTwoSortedLists {
         }
         pre = pre.next;
 
-        while (l1!=null&&l2!=null) {
-            if(l1.val<=l2.val){
-                pre.next = l1;
-                l1 = l1.next;
-            }else if(l1.val>l2.val){
+        while (l1!=null||l2!=null) {
+
+            if(l1==null&&l2 !=null){
                 pre.next = l2;
                 l2 = l2.next;
+            }else if(l2==null&&l1!=null){
+                pre.next = l1;
+                l1 = l1.next;
+            }else{
+                if(l1.val<=l2.val){
+                    pre.next = l1;
+                    l1 = l1.next;
+                }else if(l1.val>l2.val){
+                    pre.next = l2;
+                    l2 = l2.next;
+                }
             }
+
             pre = pre.next;
 
         }
-        if (l1 != null) {
-            pre.next = l1;
-        }
-        if (l2 != null) {
-            pre.next = l2;
-        }
+
         return dummyNode.next;
 
     }
