1d5bedfc50bfe3a185b667a103d1fc1da88f99b1
edsonw <969693577@qq.com>
2018-02-28 17:23:48 +0800

    leetcode 172 easy

--- src/FactorialTrailingZeroes.java
+++ src/FactorialTrailingZeroes.java
@@ -1,10 +1,15 @@
 /**
  * Created by woo23 on 2018/2/28.
- * 通过计算5的个数来判断末尾0的个数。
+ * 通过计算阶乘数字当中5的个数来判断0的个数。
+ * 比如计算10的阶乘 当中5 和10 存在两个5那就说明有2个0
+ * 如果是25那么25/5=5说明有6到25当中有5个5，然后继续计算0-5当中有几个5
+ *
+ * 如果使用传统的方法会导致存放不下阶乘结果过大的和。
  */
 public class FactorialTrailingZeroes {
     public int trailingZeroes(int n) {
-        /*double total=1;
+        //       传统思路
+        /* double total=1;
         int count=0;
         for(int i=1;i<=n;i++){
             total = total*i;
@@ -13,7 +18,6 @@ public class FactorialTrailingZeroes {
                 total= total/5;
             }
         }
-
         return count;*/
         int count = 0;
         while (n>4){
