67a131b91c1b3859ebcd9f39a4f551a0a892c32a
edsonw <969693577@qq.com>
2018-05-27 21:29:47 +0800

    动态规划

---
+++ src/Triangle120.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Triangle120
+{
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int size = triangle.size();
+        List<Integer> retlist = new ArrayList<Integer>();
+        for(int i=0;i<size;i++){
+            retlist=helper(i,triangle,retlist);
+        }
+        int minnum = 9999;
+        for(int num:retlist){
+            minnum = Math.min(num,minnum);
+        }
+        return minnum;
+    }
+    public static List<Integer> helper (int level,List<List<Integer>> triangle,List<Integer> minlevel){
+        if(level==0){
+            return triangle.get(0);
+        }
+        List<Integer> retlist = new ArrayList<Integer>();
+        int size = triangle.get(level).size();
+        for(int i=0;i<size;i++){
+            if(i==0){
+                retlist.add(triangle.get(level).get(i)+minlevel.get(i));
+            }else if(i==size-1){
+                retlist.add(triangle.get(level).get(i)+triangle.get(level-1).get(i-1));
+            }else{
+                retlist.add(Math.min(triangle.get(level).get(i)+minlevel.get(i),triangle.get(level).get(i)+minlevel.get(i-1)));
+            }
+        }
+        return retlist;
+    }
+}
