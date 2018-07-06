98a9f47d760073aae0463acf4b9ca0dddd69c0e9
969693577@qq.com <969693577@qq.com>
2018-07-06 23:28:01 +0800

    在旋转数组当中查找一个数

---
+++ src/SearchinRotatedSortedArray.java
@@ -0,0 +1,30 @@
+import java.security.cert.TrustAnchor;
+
+/**
+ * Created by edson on 2018/7/6.
+ */
+public class SearchinRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int start = 0,end = nums.length-1;
+        if(end<0)
+            return -1;
+        while (start<=end){
+            int mid = (end-start)/2+start;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<nums[end]){
+                    if(nums[mid]<target&&nums[end]>target){
+                        start = mid+1;
+                    }else
+                        end = mid-1;
+
+            }else if(nums[mid]>nums[end]){
+                if(nums[mid]>target&&nums[start]>target){
+                    end = mid-1;
+                }else
+                    start = mid+1;
+            }
+        }
+        return -1;
+    }
+}
