e5422dfe85b8abd693e4f8d5bba1a22190e5d871
969693577@qq.com <969693577@qq.com>
2018-07-07 02:04:44 +0800

    考虑到要查找的数在刚好的在边界的情况

--- src/SearchinRotatedSortedArray.java
+++ src/SearchinRotatedSortedArray.java
@@ -13,13 +13,13 @@ public class SearchinRotatedSortedArray {
             if(nums[mid]==target)
                 return mid;
             else if(nums[mid]<nums[end]){
-                    if(nums[mid]<target&&nums[end]>target){
+                    if(nums[mid]<target&&nums[end]>=target){
                         start = mid+1;
                     }else
                         end = mid-1;
 
             }else if(nums[mid]>nums[end]){
-                if(nums[mid]>target&&nums[start]>target){
+                if(nums[mid]>target&&nums[start]>=target){
                     end = mid-1;
                 }else
                     start = mid+1;
